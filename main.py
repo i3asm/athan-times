b39681de94fc7e0c2293d69f165178af6f708d21
i3asm <i3asmmail@gmail.com>
2020-07-16 18:06:32 +0300

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import pprint
+import re
+
+file = "data.json"
+
+r = requests.get('http://api.aladhan.com/v1/timings/1398332113?latitude=51.508515&longitude=-0.1254872&method=4')
+
+with open(file, 'w') as filetowrite:
+    filetowrite.write(r.text)
