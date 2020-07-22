62a73adee49cddd0ab33694220458e6c3e5b7388
i3asm <i3asmmail@gmail.com>
2020-07-22 16:03:41 +0300

    [sync]

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
 import pprint
 import re
+import json
+import datetime
+
+date = datetime.datetime.now()
 
 file = "data.json"
+api = "http://api.aladhan.com/v1/calendarByCity?city=Riyadh&country=Saudi arabia&method=4&month="+str(date.month)+"4&year="+str(date.year)
 
-r = requests.get('http://api.aladhan.com/v1/timings/1398332113?latitude=51.508515&longitude=-0.1254872&method=4')
+r = requests.get(api)
+
+loaded_json = json.loads(r.text)
+print(loaded_json)
 
 with open(file, 'w') as filetowrite:
     filetowrite.write(r.text)
-
