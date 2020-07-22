b87fc5554fe029c8ea957c9b51f8a59724f8fdc3
i3asm <i3asmmail@gmail.com>
2020-07-22 18:40:04 +0300

    refactoring and adding features

--- main.py
+++ main.py
@@ -1,18 +1,61 @@
 import requests
-import pprint
-import re
 import json
 import datetime
 
-date = datetime.datetime.now()
+# file = "data.json"
+# api = " http://api.aladhan.com/v1/timingsByCity?city=Riyadh&country=Saudi Arabia&method=4"
+# r = requests.get(api)
+# with open(file, 'w') as filetowrite:
+#     filetowrite.write(r.text)
+# loaded_json = json.loads(r.text)
+# print(loaded_json["data"]["timings"])
 
-file = "data.json"
-api = "http://api.aladhan.com/v1/calendarByCity?city=Riyadh&country=Saudi arabia&method=4&month="+str(date.month)+"4&year="+str(date.year)
+class salah:
+    def __init__(self, name, time):
+        self.name = name
+        self.time = time
 
-r = requests.get(api)
+# get salah from json
+def getSalah(name, json):
+    return json["data"]["timings"][name]
 
-loaded_json = json.loads(r.text)
-print(loaded_json)
+# format timedelta to have negative values
+def format_timedelta(td):
+    if td < datetime.timedelta(0):
+        return '-' + format_timedelta(-td)
+    else:
+        return str(td)
 
-with open(file, 'w') as filetowrite:
-    filetowrite.write(r.text)
+# get time delta
+def delta(salah):
+    time = datetime.datetime.now().strftime("%H:%M")
+    format = '%H:%M'
+    return datetime.datetime.strptime(time, format) - datetime.datetime.strptime(salah, format)
+
+# find the closest salah, and whether it's remaining time or past time
+def closest(salawat):
+    time = datetime.datetime.now().strftime("%H:%M")
+    for salah in salawat:
+        print(salah.name + ": " + salah.time)
+        if (salah.time - time > 0):
+            print("less")
+
+
+file = open("data.json", "r")
+loaded_json = json.load(file)
+
+fajr = salah(name="fajr", time=format_timedelta(delta(getSalah("Fajr", loaded_json))))
+dhuhr = salah(name="dhuhr", time=format_timedelta(delta(getSalah("Dhuhr", loaded_json))))
+asr = salah(name="asr", time=format_timedelta(delta(getSalah("Asr", loaded_json))))
+maghrib = salah(name="maghrib", time=format_timedelta(delta(getSalah("Maghrib", loaded_json))))
+isha = salah(name="isha", time=format_timedelta(delta(getSalah("Isha", loaded_json))))
+
+# print("fajr: " + format_timedelta(delta(getSalah("Fajr", loaded_json))))
+# print("Dhuhr: " + format_timedelta(delta(getSalah("Dhuhr", loaded_json))))
+# print("Asr: " + format_timedelta(delta(getSalah("Asr", loaded_json))))
+# print("Maghrib: " + format_timedelta(delta(getSalah("Maghrib", loaded_json))))
+# print("Isha: " + format_timedelta(delta(getSalah("Isha", loaded_json))))
+
+salawat = {fajr, dhuhr, asr, maghrib, isha}
+
+closest(salawat)
